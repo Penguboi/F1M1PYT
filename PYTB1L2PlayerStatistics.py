b31230ad0ecb78fff05a6332ad33b6f7b7675943
Rico <34033@MA-Web.nl>
2021-11-04 10:46:11 +0100

    playerstatistics

---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,17 @@
+print("Name: Ronald McDonald")
+print("Ranged: Hamburger")
+fries = 20
+print("Total fries: ", fries)
+health = 1600
+print("Health:", health)
+damage = 2800
+print("Damage:", damage)
+speed = 400.5
+print("Speed:", speed)
+clown = True
+print("Clown:", clown)
+print("Ablity: Ketchup Wave")
+burger_king = False
+print("Burger King:", burger_king)
+alive = False
+print("Alive:", alive)
